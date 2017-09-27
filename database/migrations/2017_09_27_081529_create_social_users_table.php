8c9334001b770f47e4705afa43475eed4fc11aad
Lester Ariel Mesa <lesterm@lestermesa.me>
2017-09-27 05:28:07 -0400

    users seeder factory

--- database/migrations/2017_09_27_081529_create_social_users_table.php
+++ database/migrations/2017_09_27_081529_create_social_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class SocialUsers extends Migration
+class CreateSocialUsersTable extends Migration
 {
     /**
      * Run the migrations.
